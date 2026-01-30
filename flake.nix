4add915715aeb6123c8e961c234d077becf4d76f
EnigmaCurry <ryan@enigmacurry.com>
2026-01-30 20:50:12 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "script-wizard - interactive prompts for shell scripts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "script-wizard";
+            version = "0.1.34";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            meta = with pkgs.lib; {
+              description = "Shell script helper for interactive prompts";
+              homepage = "https://github.com/EnigmaCurry/script-wizard";
+              license = licenses.mit;
+              maintainers = [ ];
+              mainProgram = "script-wizard";
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+            rustfmt
+          ];
+        };
+      }
+    );
+}
